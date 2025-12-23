aa4b1e758a4c18b4687eb27030c0eb341853559c
秉準 崔 <choibj@ais-info.co.jp>
2025-12-23 11:39:37 +0900

    github page host setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/nn-book/',  // GitHub repository name
   root: '.',
   publicDir: 'assets',
   server: {
