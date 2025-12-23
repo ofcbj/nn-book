f7cabe51224814cc5425065e08634f9725e63e7b
秉準 崔 <choibj@ais-info.co.jp>
2025-12-23 11:54:41 +0900

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nn-book/',  // GitHub repository name
-  root: '.',
-  publicDir: 'assets',
+  base: '/nn-book/',
   server: {
     port: 8000,
     open: true
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
   }
 });
